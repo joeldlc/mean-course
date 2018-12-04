0ca8cbe353d177aa5f2984dcb1811c31c62d5408
Joel De La Cruz <jdela1740@gmail.com>
2018-12-03 23:57:16 -0500

    Post Create finished, adds to list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {MatButtonModule, MatCardModule, MatInputModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatInputModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PostCreateComponent} from './posts/posts-create/post-create.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './header/header.component';
+import {PostListComponent} from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    HeaderComponent,
+    PostCreateComponent,
+    PostListComponent
   ],
   imports: [
     AppRoutingModule,
@@ -20,7 +24,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     FormsModule,
     MatButtonModule,
     MatCardModule,
+    MatExpansionModule,
     MatInputModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
